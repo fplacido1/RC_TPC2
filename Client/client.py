440f5b7284fa737aaa30830e6bb180cc74138a9b
Joao Norberto <jd.norberto@campus.fct.unl.pt>
2023-10-11 12:45:24 +0000

    hnged client

--- Client/client.py
+++ Client/client.py
@@ -1,19 +1,19 @@
 import socket
 import sys
 
-def execute_request(socket, server_address_port, fileName):
+def execute_request(socket, server_address_port, fileName, chunkSize):
     
 
 def main():
     n = len(sys.argv) - 1
 
-    if n != 3:
+    if n != 4:
         print("Invalid number of arguments!")
         sys.exit(-1)
 
     server_address_port = (sys.argv[1], int(sys.argv[2]))
     udp_socket_cl = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-    execute_request(udp_socket_cl, server_address_port, sys.argv[3])
+    execute_request(udp_socket_cl, server_address_port, sys.argv[3], sys.argv[4])
 
 main()
